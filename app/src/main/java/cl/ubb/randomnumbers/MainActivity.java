0624c7b2783c6c1fdb027f9e383fcd04472d4ee1
Alumno <zneel.yevenes@gmail.com>
2020-02-27 13:12:42 -0300

    Aplicacion numeros randomicos terminada

--- app/src/main/java/cl/ubb/randomnumbers/MainActivity.java
+++ app/src/main/java/cl/ubb/randomnumbers/MainActivity.java
@@ -2,6 +2,11 @@ package cl.ubb.randomnumbers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instanciar los elementos a utilizar
+
+        Button botonRandomico = (Button) findViewById(R.id.botonRandom);
+
+        final ImageView primerNumero = (ImageView) findViewById(R.id.primerNumero);
+        final ImageView segundoNumero = (ImageView) findViewById(R.id.segundoNumero);
+
+        // Obtener los identificadores que estan en draw que son enteros
+        final int [] numerosId = {
+                R.drawable.cero,
+                R.drawable.uno,
+                R.drawable.dos,
+                R.drawable.tres,
+                R.drawable.cuatro,
+                R.drawable.cinco,
+                R.drawable.seis,
+                R.drawable.siete,
+                R.drawable.ocho,
+                R.drawable.nueve
+        };
+
+
+
+        // funcion randomica para ir cambiando de imagenes
+        botonRandomico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int numeroUno = random.nextInt(10);
+                int numeroDos = random.nextInt(10);
+
+                primerNumero.setImageResource(numerosId[numeroUno]);
+                segundoNumero.setImageResource(numerosId[numeroDos]);
+
+            }
+        });
+
     }
 }
